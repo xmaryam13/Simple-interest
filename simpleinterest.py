96fd6ed2dbebdabf96398a4e33eb1d8a650c0a40
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-06-25 11:55:25 +0100

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -12,8 +12,12 @@ def calculate_interest():
     t = float(time.get())
     i = (p*r*t) / 100
     interest = round(i,2)
+    message = Label(result_frame,text='Interest' +str(p)+ 'at rate of interest' +str(r)+ 'for' +str(t)+ 'years is' +str(interest), bg='white',font=('Calibri',12),width=33)
+    message.place(x=20,y=40)
+    message.pack()
 
 show_result.destroy()
+
 
 heading_label= Label(window,text='Simple Interest Calculator',fg='black',bg='white',font=('Calibri',20),bd=5)
 heading_label.place(x=50,y=20)
@@ -47,15 +51,5 @@ show_result = Label(result_frame,text=' ',bg='white',font= ('Calibri',12),width=
 show_result.place(x=20,y=20)
 show_result.pack()
 
-def calculate_interest():
-    p = float(principle.get())
-    r = float(rate.get())
-    t = float(time.get())
-    i = (p*r*t) / 100
-    interest = round(i,2)
+window.mainloop()
 
-show_result.destroy()
-
-message = Label(result_frame,text='Interest' +str(p)+ 'at rate of interest' +str(r)+ 'for' +str(t)+ 'years is' +str(interest), bg='white',font=('Calibri',12),width=33)
-message.place(x=20,y=40)
-message.pack()
