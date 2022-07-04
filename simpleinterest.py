63f377f86743eccdaf6a93104c38db1b683b4484
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-07-04 20:25:30 +0100

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -12,11 +12,11 @@ def calculate_interest():
     t = float(time.get())
     i = (p*r*t) / 100
     interest = round(i,2)
+    show_result.destroy()
+
     message = Label(result_frame,text='Interest' +str(p)+ 'at rate of interest' +str(r)+ 'for' +str(t)+ 'years is' +str(interest), bg='white',font=('Calibri',12),width=33)
     message.place(x=20,y=40)
     message.pack()
-
-show_result.destroy()
 
 
 heading_label= Label(window,text='Simple Interest Calculator',fg='black',bg='white',font=('Calibri',20),bd=5)
